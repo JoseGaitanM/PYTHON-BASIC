9553e6be0471d843933c38fa6e089f51e68a371a
Jose Gaitan <josegaitan@Joses-MacBook-Pro.local>
2022-12-19 17:31:30 -0600

    Pyhon basics part 3 activities: task_1, task_3, task_4 done

--- practice/4_python_part_3/task_3.py
+++ practice/4_python_part_3/task_3.py
@@ -10,12 +10,26 @@ Note that address may have several domain levels
     False
 """
 import re
+import pytest 
 
 
 def is_http_domain(domain: str) -> bool:
-    ...
+    val = re.search("^http:|https:.*/$", domain)
+    if val:
+        return True
+    else:
+        return False
 
 
 """
 write tests for is_http_domain function
 """
+
+def test_http_domain():
+    assert is_http_domain('http://wikipedia.org') == True
+
+def test_https_domain():
+    assert is_http_domain('https://ru.wikipedia.org/') == True
+
+def test_fake_domain():
+    assert is_http_domain('griddynamics.com') == False
